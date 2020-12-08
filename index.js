4c6a2f98d9a76212202bfbf3669616e5326ad3b0
umair ali <umairali2k181@gmail.com>
2020-12-08 10:06:08 +0500

    message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan=require("morgan");
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
-mongoose.connect("mongodb://localhost:27017/Drivu",{ useNewUrlParser: true },{ useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://umair:umair@12345@cluster0.dfljm.mongodb.net/Ecommerce?retryWrites=true&w=majority",{ useNewUrlParser: true },{ useUnifiedTopology: true });
 mongoose.Promise=global.Promise;
 const userRouter=require("./routes/user");
 const products=require("./routes/products");
